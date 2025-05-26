41846704d8a11ca12066961176368dffea99bb28
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-26 23:20:47 +0200

    add create customer and lookup customer methods

--- myCode/Bank.cpp
+++ myCode/Bank.cpp
@@ -1,1 +1,20 @@
 #include "Bank.h"
+
+Customer* Bank::createCustomer()
+{
+    Customer* customer(nextCustomerNumber);
+    std::unique_ptr<Customer> customerPtr = std::make_unique<Customer>(customer);
+    customers.emplace(customerPtr);
+    nextCustomerNumber++;
+    return customer;
+}
+
+Customer* Bank::lookupCustomer(std::string id) const
+{
+    return customers[id];
+}
+
+std::vector<Customer*> Bank::allCustomers() const
+{
+
+}
