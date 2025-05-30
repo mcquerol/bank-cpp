6de017e7a8ad8c3296de046a3cde07643d36d666
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-30 19:49:17 +0200

    implement empty methods

--- myCode/Account.cpp
+++ myCode/Account.cpp
@@ -1,1 +1,29 @@
 #include "Account.h"
+
+Account::Account(Customer *customer, Bank::AccountType type)
+{
+}
+
+Bank::AccountType Account::accountType(std::string accountId)
+{
+}
+
+Account::~Account()
+{
+}
+
+std::string Account::getId() const
+{
+}
+
+float Account::getBalance() const
+{
+}
+
+void Account::transact(float amount)
+{
+}
+
+Poco::JSON::Object::Ptr Account::toJson() const
+{
+}
